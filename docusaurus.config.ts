549807712bb2df7b53a47c70de6e99dff4439ed6
eshanized <m.eshanized@gmail.com>
2024-12-21 06:03:23 +0530

    ✨ style(nDef): some extra changes with ui*

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,8 +2,6 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 const config: Config = {
   title: 'PlayBook | SNIGDHA OS',
   tagline: 'The official Documentation of Snigdha OS',
@@ -11,28 +9,22 @@ const config: Config = {
 
   // Set the production url of your site here
   url: 'https://snigdhaos.org/',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/playbook/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Snigdha-OS', // Usually your GitHub org/user name.
-  projectName: 'playbook', // Usually your repo name.
+  organizationName: 'Snigdha-OS', // GitHub org/user name
+  projectName: 'playbook', // Repo name
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
+  // Localization settings
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'hi', 'bn'],
+    locales: ['en', 'hi', 'bn'], // English, Hindi, Bengali
     localeConfigs: {
       en: { label: 'English' },
-      fr: { label: 'Français' },
-      es: { label: 'Español' },
+      hi: { label: 'हिन्दी' },
+      bn: { label: 'বাংলা' },
     },
   },
 
@@ -42,25 +34,9 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/Snigdha-OS/playbook/tree/master/',
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          sidebarCollapsible: false,
+          routeBasePath: '/', // Set docs as the homepage
+          editUrl: 'https://github.com/Snigdha-OS/playbook/tree/master/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -70,24 +46,20 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/docusaurus-social-card.jpg', // Social media preview image
     navbar: {
       title: 'PlayBook | SNIGDHA OS',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Snigdha OS Logo',
         src: 'img/logo.svg',
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
+          type: 'localeDropdown', // Language switcher
+          position: 'right',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/Snigdha-OS/',
           label: 'GitHub',
           position: 'right',
         },
@@ -95,48 +67,9 @@ const config: Config = {
     },
     footer: {
       style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      // Remove all footer links
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} Snigdha OS. Powered by Tonmoy Infrastructure.`,
     },
     prism: {
       theme: prismThemes.github,
